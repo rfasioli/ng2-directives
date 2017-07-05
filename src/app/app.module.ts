c04ae081648599a0cd17cdbc8f15a019a009e84c
Rodrigo Fasioli <rodrigo.fasioli@gmail.com>
2017-07-05 19:35:43 -0300

    Versao inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
+import { TesteComponent } from './teste/teste.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiretivaNgifComponent,
+    TesteComponent
   ],
   imports: [
     BrowserModule
